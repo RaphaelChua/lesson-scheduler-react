d6a53952aaf375893525a9ca2e1ed1722ba7b34e
rap <raphaelvguy@gmail.com>
2021-08-02 22:23:50 +0800

    add new changes

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  MonthView,
+  Appointments,
+} from "@devexpress/dx-react-scheduler-material-ui";
+
+// import { appointments } from "../../../demo-data/month-appointments";
+
+const currentDate = "2018-07-17";
 
 const Main = () => {
-  return <div>hi</div>;
+  return (
+    <Paper>
+      <Scheduler>
+        <ViewState currentDate={currentDate} />
+        <MonthView />
+        <Appointments />
+      </Scheduler>
+    </Paper>
+  );
 };
 
 export default Main;
