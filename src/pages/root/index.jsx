53c928c5a79f107a8c0a1ea6b2887db72e9c30b2
rap <raphaelvguy@gmail.com>
2021-08-03 19:38:52 +0800

    add Login page

--- src/pages/root/index.jsx
+++ src/pages/root/index.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Routes from "./routes";
 import TopAppBar from "./topappbar";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import Login from "../login";
 
-const Root = () => {
+const DefaultContainer = () => {
   return (
     <div>
       <TopAppBar />
       <Routes />
     </div>
+  );
+};
+
+const Root = () => {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route component={DefaultContainer} />
+      </Switch>
+    </HashRouter>
   );
 };
 
--- src/pages/root/routes.jsx
+++ src/pages/root/routes.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from "react";
 
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 const login = lazy(() => import("../login"));
 
@@ -8,16 +8,14 @@ const main = lazy(() => import("../main"));
 
 const Routes = () => {
   return (
-    <div>
-      <HashRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/main" component={main} />
-            <Route path="/login" component={login} />
-          </Switch>
-        </Suspense>
-      </HashRouter>
-    </div>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/main" component={main} />
+          <Route path="/login" component={login} />
+        </Switch>
+      </Suspense>
+    </>
   );
 };
 
